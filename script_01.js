2bb56e73d4e972bc658a5bc0a9c7c36f8f25be0e
Peter <schmidt.multimedia@gmail.com>
2022-11-08 12:06:41 +0100

    02 Deklaration + Wertzuweisung I

--- script_01.js
+++ script_01.js
@@ -14,4 +14,12 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+
 
